9fa2d0394bb46a9b8d0e915a9393affa696a4738
Polina <morozova_p_v@mail.ru>
2023-04-16 18:10:17 +0300

    Config work well

--- src/Types/ExceptionTypes.hs
+++ src/Types/ExceptionTypes.hs
@@ -7,6 +7,7 @@
 module Types.ExceptionTypes where
 
 import qualified Control.Exception.Safe as EXS
+import qualified Data.Text as Т
 import Logger (logError, (.<))
 import qualified News
 import qualified System.Exit as Exit
@@ -18,12 +19,15 @@ data ServerException
     MigrationError String
   | -- | NotConfig -- ERROR throw when  did not load config file
     NotConfig EXS.SomeException
+  | -- | NotConfig -- ERROR throw when did not look up field in the given config
+    NotConfigField Т.Text
   deriving (EXS.Exception)
 
 instance Show ServerException where
-  show (DbNotConnect msg) = "Failed, not connection to the Data Base!  " ++ show msg
-  show (MigrationError msg) = "Failed, during migrations!  " ++ show msg
-  show (NotConfig msg) = "Failed, did not load config file! " ++ show msg
+  show (DbNotConnect msg) = "Failed, not connection to the Data Base!  " <> show msg
+  show (MigrationError msg) = "Failed, during migrations!  " <> msg
+  show (NotConfig msg) = "Failed, did not load config file! " <> show msg
+  show (NotConfigField name) = "Failed, did not look up correct value in the field " <> show name <> " in the config file! "
 
 handleExceptionToLog :: News.Handle IO -> ServerException -> IO a
 handleExceptionToLog h e = do
@@ -32,5 +36,5 @@ handleExceptionToLog h e = do
 
 handleExceptionToTerminal :: ServerException -> IO a
 handleExceptionToTerminal e = do
-  putStrLn $ "catch ServerException: ERROR! " ++ show e
+  putStrLn $ "catch ServerException: ERROR! " <> show e
   Exit.exitFailure
